a05611cb62ce650c9164f64326fc6b3646703d33
developcoding <omidvarisl@gmx.de>
2026-05-12 13:55:12 +0200

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Checkout from './pages/Checkout'
+import Navbar from './components/Navbar'
+
 
 function App() {
   
 
   return (
-   <>
-    <h1>Data Fetch</h1>
-   </>
+      <div className='app'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/auth'element={<Auth/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
+        </Routes>
+      </div>
   )
 }
 
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+const ProductCard = ({image,name,price}) => {
+  return (
+    <div className='product-card'>
+                            <img src={image} className='product-card-image'/>
+                            <div className="product-card-content">
+                                <h3 className='product-card-name'>{name}</h3>
+                                <p className='product-card-price'>${price}</p>
+                                <div className='product-card-actions'>
+                                    <Link className='btn btn-secondary'>View Details</Link>
+                                    <button className='btn btn-primary'>Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+  )
+}
+
+export default ProductCard

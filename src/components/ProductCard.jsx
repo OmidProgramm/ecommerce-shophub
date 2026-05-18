b17076d42353b0cc1d0e39d31c57950d5aa3506c
developcoding <omidvarisl@gmx.de>
2026-05-18 14:48:44 +0200

    feat: finished add to cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-const ProductCard = ({id,image,name,price}) => {
+import { useCart } from '../context/CartContext';
+const ProductCard = (product) => {
+    const {cartItems,addToCart} = useCart();
+    const productInCart = cartItems.find((item)=>item.id === product.id);
+    const productQuantityLabel = productInCart? `(${productInCart.quantity})`:'';
   return (
-    <div className='product-card'>
-                            <img src={image} alt={name} className='product-card-image'/>
-                            <div className="product-card-content">
-                                <h3 className='product-card-name'>{name}</h3>
-                                <p className='product-card-price'>${price}</p>
-                                <div className='product-card-actions'>
-                                    <Link className='btn btn-secondary' to= {`/products/${id}`} >View Details</Link>
-                                    <button className='btn btn-primary'>Add to Cart</button>
-                                </div>
-                            </div>
-                        </div>
+            <div className='product-card'>
+                <img src={product.image} alt={name} className='product-card-image'/>
+                <div className="product-card-content">
+                    <h3 className='product-card-name'>{product.name}</h3>
+                    <p className='product-card-price'>${product.price}</p>
+                    <div className='product-card-actions'>
+                        <Link className='btn btn-secondary' to= {`/products/${product.id}`} >View Details</Link>
+                        <button className='btn btn-primary' onClick={()=>addToCart(product.id)}>Add to Cart {productQuantityLabel}</button>
+                    </div>
+                </div>
+            </div>
   )
 }
 
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useCart } from '../context/CartContext'
 
 const Checkout = () => {
+  const {} = useCart();
   return (
-    <div>Checkout</div>
+    <div className='page'>
+      <div className="container">
+        <h1 className="page-title">Checkout</h1>
+        <div className="checkout-container">
+          <div className="checkout-items">
+            <h2>Order Summary</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

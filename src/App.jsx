971f9f9058c34c81dfced8dadb05b181c0c0528c
developcoding <omidvarisl@gmx.de>
2026-05-19 17:00:32 +0200

    ready for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <Navbar/>
           <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/auth'element={<Auth/>}/>
+            <Route path='/auth' element={<Auth/>}/>
             <Route path='/checkout' element={<Checkout/>}/>
             <Route path='/products/:id' element={<ProductDetails/>}/>
           </Routes>
